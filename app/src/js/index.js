aebd5f35d6004f7d65bcd856ec6a6ca7ff8c7f1f
zidong.wang <zidong.wang@qunar.com>
2017-04-25 19:34:20 +0800

    init mvc

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,4 +1,21 @@
 "use strict";
-$(document).ready(function(){
-    $('title').html("这是一个关于jq的测试页");
-});
+var GV = require('./mvvm/GV');
+$(function(){
+    var _gv = new GV({
+        el:"test",
+        data:function(){
+            return {
+                name:"王子冬",
+                sex:"男",
+                mum:{
+                    name:"美女",
+                    sex:"girl"
+                }
+            }
+        }
+    });
+    setTimeout(function(){
+        _gv.$data.name = "大拽哥";
+        _gv.$data.mum.name = "大拽姐";
+    },2000);
+})
---
+++ app/src/js/mvvm/GV.js
@@ -0,0 +1,70 @@
+"use strict";
+var HClass = require('../base/HClass');
+
+var valREG = /{{(.+?)}}/gi;
+
+var GV = HClass.extend({
+    $id:null,
+    $dom:null,
+    $data:null,
+    $tpl:null,
+
+    ctor:function(options){
+        this.$id = options&&options.el;
+        this.$data = options&&options.data&&options.data()||{};
+        this.compileDom();
+        this.compileData(this.$data);
+        this.render();
+    },
+    compileDom:function(){
+        var id = this.$id;
+        var dom = this.$dom = document.getElementById(id);
+        var innerHtml = dom.innerHTML;
+        var arr = valREG.exec(innerHtml);
+        var tpl = 'return ',lastIndex=0;
+        for(;arr;arr = valREG.exec(innerHtml)){
+            console.log(arr);
+            var preStr = innerHtml.substring(lastIndex,arr.index);
+            lastIndex = valREG.lastIndex;
+            tpl+='\''+preStr.replace(/\'/g,'\\\'')+'\''+'+data.'+arr[1]+'+';
+        }
+        var afterStr = innerHtml.substring(lastIndex);
+        tpl+='\''+afterStr.replace(/\'/g,'\\\'')+'\'';
+        this.$tpl = new Function('data',tpl.replace(/\n/g,''));
+    },
+
+    _compileObj:function(obj,key,defaultVal){
+        var _this = this;
+        var val = defaultVal;
+        Object.defineProperty(obj, key, {
+            configurable: true,
+            enumerable: true,
+            set: function(name) {
+                val = name;
+                if(typeof name=="object"){
+                    _this.compileData(name);
+                }
+                _this.render();
+            },
+            get: function() {
+                return val;
+            }
+        });
+    },
+    compileData:function(data){
+        for(var key in data){
+            var val = data[key];
+            if(typeof val == "object"){
+                this.compileData(val);
+            }
+            this._compileObj(data,key,data[key]);
+        }
+    },
+    render:function(){
+        console.log(this.$data);
+        var html = this.$tpl(this.$data);
+        this.$dom.innerHTML = html;
+    }
+
+});
+module.exports = GV;
