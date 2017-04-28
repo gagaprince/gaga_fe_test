e5689a51df4bdf8502c2272b9bf9442ead3546dd
zidong.wang <wangzidongprince@163.com>
2017-04-28 23:18:19 +0800

    add doT

---
+++ app/src/js/mvvm/GVForDot.js
@@ -0,0 +1,73 @@
+"use strict";
+var HClass = require('../base/HClass');
+var doT = require('../../../../bower_components/doT/doT.js');
+
+
+
+var GV = HClass.extend({
+    $id:null,
+    $dom:null,
+    $data:null,
+    $tpl:null,
+    _renderlock:null,
+
+    ctor:function(options){
+        this.$id = options&&options.el;
+        this.$data = options&&options.data&&options.data()||{};
+        this.compileDom();
+        this.compileData(this.$data);
+        this.render();
+        console.log(doT);
+    },
+    compileDom:function(){
+        var id = this.$id;
+        var dom = this.$dom = document.getElementById(id);
+        var innerHtml = dom.innerHTML;
+        this.$tpl = doT.template(innerHtml);
+    },
+
+    _compileObj:function(obj,key,defaultVal){
+        var _this = this;
+        var val = defaultVal;
+        Object.defineProperty(obj, key, {
+            configurable: true,
+            enumerable: true,
+            set: function(name) {
+                val = name;
+                if(typeof name=="object"){
+                    _this.compileData(name);
+                }
+                _this.postRender();
+            },
+            get: function() {
+                return val;
+            }
+        });
+    },
+    compileData:function(data){
+        for(var key in data){
+            var val = data[key];
+            if(typeof val == "object"){
+                this.compileData(val);
+            }
+            this._compileObj(data,key,data[key]);
+        }
+    },
+    postRender:function(){
+        if(this._renderlock){
+            clearTimeout(this._renderlock);
+            this._renderlock = null;
+        }
+        var _this = this;
+        this._renderlock = setTimeout(function(){
+            _this.render();
+        });
+    },
+    render:function(){
+//        console.log(this.$data);
+        var html = this.$tpl(this.$data);
+        this.$dom.innerHTML = html;
+    }
+
+});
+module.exports = GV;
---
+++ app/src/js/testDot.js
@@ -0,0 +1,21 @@
+"use strict";
+var GV = require('./mvvm/GVForDot');
+$(function(){
+    var _gv = new GV({
+        el:"test",
+        data:function(){
+            return {
+                name:"王子冬",
+                sex:"男",
+                mum:{
+                    name:"美女",
+                    sex:"girl"
+                }
+            }
+        }
+    });
+    setTimeout(function(){
+        _gv.$data.name = "大拽哥";
+        _gv.$data.mum.name = "大拽姐";
+    },2000);
+})
