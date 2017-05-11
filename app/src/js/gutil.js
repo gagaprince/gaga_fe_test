fa8e4e09ee47082be470275f5dd065bd73b9c91a
zidong.wang <wangzidongprince@163.com>
2017-05-11 20:10:28 +0800

    add some function

--- app/src/js/gutil.js
+++ app/src/js/gutil.js
@@ -4,11 +4,22 @@ var UrlUtil = require('./util/UrlUtil');
 var PromiseUtil = require('./util/PromiseUtil');
 var DateUtil = require('./util/DateUtil');
 var EventManager = require('./util/EventManager');
+var ArrayUtil = require('./util/ArrayUtil');
+var UaUtil = require('./util/UaUtil');
+var MD5Util = require('./util/MD5Util');
+var Base64Util = require('./util/Base64Util');
 
 var gutil = window.gu = {
     GClass:HClass
 }
-BaseUtil.extend(gutil,BaseUtil,UrlUtil,PromiseUtil,DateUtil);
+BaseUtil.extend(gutil,BaseUtil);
+BaseUtil.extend(gutil,UrlUtil);
+BaseUtil.extend(gutil,PromiseUtil);
+BaseUtil.extend(gutil,DateUtil);
 BaseUtil.extend(gutil,EventManager);
+BaseUtil.extend(gutil,ArrayUtil);
+BaseUtil.extend(gutil,UaUtil);
+BaseUtil.extend(gutil,MD5Util);
+BaseUtil.extend(gutil,Base64Util);
 
 module.exports = gutil;
